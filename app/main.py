2c1def125b73569f4053328c598242d8c4246114
artemioo <artemryb1@gmail.com>
2023-10-12 13:41:20 +0300

    make new structure of project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends
 import uvicorn
+from sqlalchemy.ext.asyncio import AsyncSession
+from app import crud
 
-from core.models import Base
-from core.models.db_session import db_helper
-
+from app.db import db_helper
+from app.schemas.book_schema import BookSchema
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -22,6 +24,15 @@ app = FastAPI(lifespan=lifespan)
 @app.get('/')
 def home():
     return {'response': 'OK'}
+
+
+# TODO: убрать это в api + router
+@app.get('/books/', response_model=list[BookSchema])
+async def get_all_books(session: AsyncSession = Depends(db_helper.session_dependency)) -> list[BookSchema]:
+    return await crud.get_books(session=session)
+
+
+
 
 
 if __name__ == '__main__':
