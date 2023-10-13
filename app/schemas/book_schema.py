bbfc128db6c8a5580213e7f8367b6250e672a639
artemioo <artemryb1@gmail.com>
2023-10-13 12:09:25 +0300

    separate Book Schema

--- app/schemas/book_schema.py
+++ app/schemas/book_schema.py
@@ -1,9 +1,19 @@
 from pydantic import Field, BaseModel, ConfigDict
 
 
-class BookSchema(BaseModel):
+class BaseBookSchema(BaseModel):
     title: str = Field(min_length=1)
     year: int = Field(gt=0)
     pages: int = Field(gt=0)
 
-    model_config = ConfigDict(from_attributes=True)  # ???
+
+class BookCreateSchema(BaseBookSchema):
+    pass
+
+
+class BookUpdateSchema(BaseBookSchema):
+    pass
+
+class BookSchema(BaseBookSchema):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
